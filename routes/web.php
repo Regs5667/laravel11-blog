10a2bc422cba420ef110574b1185bb7e2d65a1aa
Regs5667 <rohmatpacific@gmail.com>
2025-03-14 09:49:31 +0700

    custom logika middleware dan alias middleware

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\EnsureTokenIsValid;
 use App\Models\Comment;
 use App\Models\Phone;
 use Illuminate\Support\Facades\Request;
@@ -23,9 +24,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/blog/{id}/delete', [BlogController::class, 'delete']);
     Route::get('/user', [UserController::class, 'index']);
 
-    Route::get('/phone', function () {
-        return Phone::with('user')->get();
-    });
+
 
 
     Route::post('/comment/{id}', [CommentController::class, 'index'])->name('comment');
@@ -35,6 +34,10 @@ Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'AuthLogout'])->name('logout');
 });
 
+
+Route::get('/phone', function () {
+    return Phone::with('user')->get();
+})->middleware('inialias');
 Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'index'])->name('login');
     Route::post('/login', [AuthController::class, 'AuthLogin']);
